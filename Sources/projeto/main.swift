cab9e9e085e2d8be311f04191fc105dad8a618b5
Sallitos <156387335+Sallitos@users.noreply.github.com>
2025-10-24 19:02:43 +0000

    Final

--- Sources/projeto/main.swift
+++ Sources/projeto/main.swift
@@ -1,31 +1,29 @@
 import Foundation
 
-let academia:Academia = Academia(nome: "Academia POO 360", alunosMatriculados: [:], instrutoresContratados: [:], aparelhos: [], aulasDisponiveis: [])
+let academia: Academia = Academia(nome: "Academia POO 360", alunosMatriculados: [:], instrutoresContratados: [:], aparelhos: [], aulasDisponiveis: [])
 
 let planomensal: PlanoMensal = PlanoMensal()
 let planoanual: PlanoAnual = PlanoAnual()
 
-let instrutor1: Instrutor = Instrutor(nome: "Felipe Santana", email: "felipe0421@gmail.com", especialidade: "Triceps")
-let instrutor2: Instrutor = Instrutor(nome: "Bruno Wagner", email: "wagner32412@gmail.com", especialidade: "Abdutor")
+let instrutor1: Instrutor = Instrutor(nome: "Breno Correia Almeida", email: "breno1234@gmail.com", especialidade: "Perna")
+let instrutor2: Instrutor = Instrutor(nome: "Gabrielly Rocha", email: "gabrielly1234@gmail.com", especialidade: "Peito")
 
 academia.contratarInstrutor(instrutor1)
 academia.contratarInstrutor(instrutor2)
 print()
 
-let aluno1 = academia.matricularAluno(nome: "Bubens Tanaka", email: "tanaka523@gmail.com", matricula: "1234", plano: planomensal)
-let aluno2 = academia.matricularAluno(nome: "Aidia Riquelme", email: "lidgia453@hotmail.com", matricula: "1433", plano: planoanual)
+let aluno1 = academia.matricularAluno(nome: "Luiz Martins", email: "luiz1234@gmail.com", matricula: "0001", plano: planomensal)
+let aluno2 = academia.matricularAluno(nome: "Tânia Dias", email: "tania1234@gmail.com", matricula: "0002", plano: planoanual)
 print()
 
-let aula1: AulaPersonal = AulaPersonal(nome: "Peito", instrutor: instrutor1, aluno: aluno1)
-let aula2: AulaColetiva = AulaColetiva(nome: "Zumba", instrutor: instrutor2, capacidadeMaxima: 3)
+let aula1: AulaPersonal = AulaPersonal(nome: "Perna", instrutor: instrutor1, aluno: aluno1)
+let aula2: AulaColetiva = AulaColetiva(nome: "Peito", instrutor: instrutor2, capacidadeMaxima: 3)
 
 academia.adicionarAula(aula1)
 academia.adicionarAula(aula2)
 
 print("Adicionando alunos à aula coletiva")
 
-let aluno3: Aluno = academia.matricularAluno(nome: "Dose Magalhães", email: "rose9545@yahoo.com.br", matricula: "3134", plano: planomensal)
-let aluno4: Aluno = academia.matricularAluno(nome: "Cernando Miguel", email: "fefe431@outlook.com.br", matricula: "6243", plano: planoanual)
 
 if(aula2.inscrever(aluno: aluno1)){
     print("Aluno \(aluno1.nome) inscrito com sucesso em \(aluno1.nome)")
@@ -39,11 +37,14 @@ if(aula2.inscrever(aluno: aluno2)){
     print("ERRO: aluno \(aluno2.nome) não foi inscrito!")
 }
 
+let aluno3: Aluno = academia.matricularAluno(nome: "Lucas Cardoso", email: "lucas@gmail.com", matricula: "0003", plano: planomensal)
 if(aula2.inscrever(aluno: aluno3)){
     print("Aluno \(aluno1.nome) inscrito com sucesso em \(aluno3.nome)")
 } else {
     print("ERRO: aluno \(aluno3.nome) não foi inscrito!")
 }
+
+let aluno4: Aluno = academia.matricularAluno(nome: "Gustavo Correia", email: "gustavo@gmail.com", matricula: "0004", plano: planoanual)
 
 if(aula2.inscrever(aluno: aluno4)){
     print("Aluno \(aluno4.nome) inscrito com sucesso em \(aluno4.nome)")
@@ -57,6 +58,8 @@ academia.listarAlunos()
 print()
 academia.listarAulas()
 print("\n")
+
+
 var aulasLista: [Aula] = [aula1, aula2] 
 for teste in aulasLista{
     print(teste.getDescricao(),"\n")
